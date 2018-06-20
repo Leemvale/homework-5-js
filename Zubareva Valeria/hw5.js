056c208ea961fb49a295ce66491839931faa2a21
leemvale <neuromiwork@gmail.com>
2018-06-20 22:16:58 +0300

    fixed

--- Zubareva Valeria/hw5.js
+++ Zubareva Valeria/hw5.js
@@ -6,10 +6,9 @@ function MenuItem() {
     this._consist = [].slice.call(arguments);
 }
 
-MenuItem.prototype.getConsist= function () {
+MenuItem.prototype.getConsist = function () {
     return this._consist;
 };
-
 MenuItem.prototype.calculatePrice = function () {
     var consist = this.getConsist();
     var price = 0;
@@ -18,7 +17,6 @@ MenuItem.prototype.calculatePrice = function () {
     });
     return price;
 };
-
 MenuItem.prototype.calculateCalories = function () {
     var consist = this.getConsist();
     var calories = 0;
@@ -28,24 +26,28 @@ MenuItem.prototype.calculateCalories = function () {
     return calories;
 };
 
-
-function Hamburger(size, stuffing) {
-    try {
-        if (size == null || stuffing == null) {
-            throw new Exception('no arguments');
-        } else if (size != Hamburger.SIZE_SMALL && size != Hamburger.SIZE_LARGE) {
-            throw new Exception('invalid size');
+try {
+    function Hamburger(size, stuffings) {
+        if (size === undefined) {
+            throw new Exception('Missing argument "size"');
+        } else if (size !== Hamburger.SIZE_SMALL && size !== Hamburger.SIZE_LARGE) {
+            throw new Exception('Invalid size');
+        } else if (stuffings.length === 0 || stuffings === undefined) {
+            throw new Exception('Missing argument "stuffings"');
         } else {
-            MenuItem.apply(this, arguments);
+            for (var i = 0; i < stuffings.length; i++) {
+                if (stuffings[i] !== Hamburger.STUFFING_SALAD && stuffings[i] !== Hamburger.STUFFING_POTATO && stuffings[i] !== Hamburger.STUFFING_CHEESE) {
+                    throw new Exception('Invalid stuffings');
+                }
+            }
+            MenuItem.apply(this, stuffings.concat(size));
             this._size = size;
-            this._stuffing = [stuffing].concat([].slice.call(arguments, 2));
+            this._stuffing = stuffings;
         }
-    } catch (e) {
-        console.log(e.message);
     }
-
+} catch (e) {
+    console.error(e.message);
 }
-
 
 Hamburger.SIZE_SMALL = {
     'name': 'small',
@@ -79,24 +81,27 @@ Hamburger.prototype.constructor = Hamburger;
 Hamburger.prototype.getSize = function () {
     return this._size;
 };
-
 Hamburger.prototype.getStuffing = function () {
     return this._stuffing;
 };
 
-
-function Salad(type) {
-    try {
-        if (type== null) {
-            throw new Exception('no arguments');
+try {
+    function Salad(type, portion) {
+        if (type === undefined) {
+            throw new Exception('Missing argument "type"');
+        } else if (type !== Salad.OLIVIE && type !== Salad.CAESAR) {
+            throw new Exception('Incorrect argument type');
+        } else if (portion === undefined) {
+            throw new Exception('Missing argument "portion"');
         } else {
-             MenuItem.call(this, type);
+            MenuItem.call(this, type);
+            this._portion = portion;
+            this._type = type;
         }
-    } catch (e) {
-        console.log(e.message);
     }
+} catch (e) {
+    console.error(e.message);
 }
-
 Salad.CAESAR = {
     'name': 'caesar',
     'price': 100,
@@ -107,22 +112,38 @@ Salad.OLIVIE = {
     'price': 50,
     'calories': 80
 };
+Salad.STANDARTPRTION = 100;
 
 Salad.prototype = Object.create(MenuItem.prototype);
 Salad.prototype.constructor = Salad;
 
-function Drink(type) {
-    try {
-        if (type == null) {
-            throw new Exception('no arguments');
+Salad.prototype.getPortion = function () {
+    return this._portion;
+};
+Salad.prototype.getType = function () {
+    return this._type;
+};
+
+Salad.prototype.calculatePrice = function () {
+    return this.getType().price / Salad.STANDARTPRTION * this.getPortion();
+};
+
+Salad.prototype.calculateCalories = function () {
+    return this.getType().calories / Salad.STANDARTPRTION * this.getPortion();
+};
+try {
+    function Drink(type) {
+        if (type === undefined) {
+            throw new Exception('Missing argument "type"');
+        } else if (type !== Drink.COFFEE && type !== Drink.COLA) {
+            throw new Exception('Incorrect argument type');
         } else {
             MenuItem.call(this, type);
         }
-    } catch (e) {
-        console.log(e.message);
     }
+} catch (e) {
+    console.error(e.message);
 }
-
 Drink.COLA = {
     'name': 'cola',
     'price': 50,
@@ -141,18 +162,23 @@ function Order() {
     this._points = [].slice.call(arguments);
     this.paid = false;
 }
+
 Order.prototype.getPoints = function () {
     return this._points.slice(0);
 };
+try {
+    Order.prototype.setPoints = function (points) {
 
-Order.prototype.setPoints = function(points) {
-    if(!this.paid){
-        this._points = points;
-    } else {
-        console.log("Order was paid!")
+        if (this.paid) {
+            throw new Exception('Order was "payd! You cannot change it!"');
+        } else {
+            this._points = points;
+        }
+
     }
-};
-
+} catch (e) {
+    console.error(e.message);
+}
 Order.prototype.calculatePrice = function () {
     var price = 0;
     this.getPoints().forEach(function (item) {
@@ -170,7 +196,7 @@ Order.prototype.calculateCalories = function () {
 };
 
 Order.prototype.add = function (item) {
-        this.setPoints(this.getPoints().concat(item));
+    this.setPoints(this.getPoints().concat(item));
 };
 
 Order.prototype.del = function (item) {
@@ -185,29 +211,41 @@ Order.prototype.del = function (item) {
 Order.prototype.pay = function () {
     Object.defineProperty(this, '_points', {
         value: this.getPoints(),
-        configurable: false,
         writable: false
-        });
+    });
     this.paid = true;
 };
 
-function Menu(){
-    this.hamburger1 = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO);
-    this.hamburger2 = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE);
-    this.cola = new Drink(Drink.COLA);
-    this.coffe = new Drink(Drink.COFFEE);
-    this.salad1 = new Salad(Salad.OLIVIE);
-    this.salad2 = new Salad(Salad.OLIVIE);
+function Menu() {
 }
 
+Menu.prototype.hamburger = function () {
+    var size = arguments[0];
+    var stuffings = [].slice.call(arguments, 1);
+    return new Hamburger(size, stuffings);
+};
+Menu.prototype.drink = function () {
+    var type = arguments[0];
+    return new Drink(type);
+};
+Menu.prototype.salad = function () {
+    var type = arguments[0];
+    var portion = arguments[1];
+    return new Salad(type, portion);
+};
 
-var menu  = new Menu();
-var order = new Order(menu.hamburger1, menu.hamburger2, menu.cola, menu.salad1);
+var myMenu = new Menu();
+var drink1 = myMenu.drink(Drink.COLA);
+var drink2 = myMenu.drink(Drink.COFFEE);
+var salad1 = myMenu.salad(Salad.CAESAR, 200);
+var hamburger1 = myMenu.hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE, Hamburger.STUFFING_POTATO);
+var order = new Order(drink1, drink2, salad1, hamburger1);
 
-order.add(menu.salad2);
+order.add(myMenu.salad(Salad.OLIVIE, 100));
+order.del(salad1);
 
 order.pay();
 
-order.del(menu.coffe);
+// order.add(myMenu.salad(Salad.OLIVIE, 100));
+console.log(order, order.calculatePrice(), order.calculateCalories());
 
-console.log(order);
